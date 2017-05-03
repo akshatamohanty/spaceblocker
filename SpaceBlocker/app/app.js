38601b05c529e524df6875cf19da7c3ed86f39df
Balasubrahmanyam E <balu_eastamsetty@hotmail.com>
2017-05-03 17:49:12 +0800

    layoutpane Updated

--- SpaceBlocker/app/app.js
+++ SpaceBlocker/app/app.js
@@ -2,38 +2,48 @@
  * Created by sereb on 10/3/2017.
  */
 
-(function(){
+/*(function(){*/
+
+	deskArrayGlobal=null;
 	var spaceBlocker= angular.module('spaceBlocker',['ui.layout','rzModule', 'ui.bootstrap','smart-table','nvd3']);
 
 	spaceBlocker.controller("myCtrl", function($scope) {
-
 		$scope.LoadFloor = function(selectedFloor) {
 			$scope.floorImage=selectedFloor;
 
 		};
 	});
-	spaceBlocker.controller('sliderCtrl',['$rootScope', '$timeout', '$modal','$scope',function ($scope, $rootScope,$timeout) {
-
-
+	spaceBlocker.controller('sliderCtrl',['$rootScope', '$timeout', '$modal','$scope',function ($rootScope, $timeout, $modal, $scope) {
 
 		//Slider config with custom display function
 		$scope.slider_translate = {
-
+			value: 0,
 			options: {
 				stepsArray:timeline,
 				translate: function (value) {
-					$scope.sliderValue=value;
+					//var sliderValue=value;
 					var today = new Date(value);
 					return  today.toDateString();
-				},
+				}
 
 			}
 		};
 
+		var broadcast = function(){
+			$rootScope.$broadcast('sliderChanged', function(){
+				//alert("time value braodd");
+				return $scope.slider_translate.value;
+
+			})
+		}
+
+		$scope.$watch('slider_translate.value', broadcast)
+
+
+
 	}]);
 
 	spaceBlocker.controller('tableCtrl', ['$scope', function ($scope) {
-
 
 		$scope.schedule=schedule;
 
@@ -75,6 +85,7 @@
 		$scope.addRandomItem = function addRandomItem() {
 			$scope.rowCollection.push(generateItemFromSchedule());
 
+
 		};
 
 		$scope.importFromCSV=function importFromCSV() {
@@ -102,6 +113,7 @@
 							course.push(el.value);
 						});
 						$scope.rowCollection.push(generateObject(course));
+						$scope.$apply();
 					});
 				});
 			});
@@ -117,7 +129,38 @@
 
 	}]);
 
-	spaceBlocker.controller('graphCtrl', ['$rootScope','$scope',function($scope,$rootScope) {
+    spaceBlocker.controller('layoutCtrl',['$scope',function ($scope) {
+
+	$scope.timeSliderValue = 1025409600000;
+	$scope.$on('sliderChanged', function(message, data){
+		$scope.timeSliderValue = data();
+		//alert("time value updated");
+	})
+	$scope.floorList=floors;
+
+
+	// Keep a copy of the collection for tests
+	var dataCopy = JSON.parse(JSON.stringify(data));
+
+	$scope.deskArray=[];
+
+	data.map(function(obj){
+
+	 obj.values.map(function(value){
+
+	 if(value[0]===$scope.timeSliderValue){
+	 $scope.deskArray.push(value);
+	 }
+	 return value[0]===1025409600000;
+	 });
+	 });
+	$scope.$apply();
+
+	deskArrayGlobal=$scope.deskArray;
+
+}]);
+
+	spaceBlocker.controller('graphCtrl', ['$rootScope','$scope',function($rootScope,$scope) {
 
 
 		$scope.options = {
@@ -160,34 +203,25 @@
 					dispatch: {
 						elementMousemove: function(e) {
 
-							var today = new Date(e.pointXValue);
-							$scope.scrollValue=today.toDateString();
-							console.log(e.mouseX + " " + e.mouseY + " " +e.pointXValue);
+							// var today = new Date(e.pointXValue);
+							// $scope.scrollValue=today.toDateString();
+							// console.log(e.mouseX + " " + e.mouseY + " " +e.pointXValue);
 						},
 						elementClick: function(e) {
-							console.log(e.mouseX + " " + e.mouseY + " " + e.pointXValue);
+							// console.log(e.mouseX + " " + e.mouseY + " " + e.pointXValue);
 						}
 					},
 				}
 			}
 		};
 		$scope.data = data;
+		$scope.timeSliderValue = 0;
+		$scope.$on('sliderChanged', function(message, data){
+			$scope.timeSliderValue = data();
+
+		})
 
 	}]);
-
-	spaceBlocker.controller('layoutCtrl',['$scope',function ($scope) {
-
-		$scope.floorList=floors;
-
-		// Keep a copy of the collection for tests
-		var dataCopy = JSON.parse(JSON.stringify(data));
-
-		$scope.deskArray=data;
-
-
-	}]);
-
-	var sliderValue= null;
 
 	var floors=[
 		{
@@ -227,7 +261,7 @@
 		}
 	];
 
-	var data = [
+	 var data = [
 		{
 			"key" : "First Year" ,
 			"values" : [ [ 1025409600000 , 10] , [ 1028088000000 , 9] , [ 1030766400000 , 11] , [ 1033358400000 , 12] , [ 1036040400000 , 15] , [ 1038632400000 , 16] , [ 1041310800000 , 9] , [ 1043989200000 , 9] , [ 1046408400000 , 9] , [ 1049086800000 , 11] , [ 1051675200000 , 14] , [ 1054353600000 , 14] , [ 1056945600000 , 13] , [ 1059624000000 , 13] , [ 1062302400000 , 14] , [ 1064894400000 , 14] , [ 1067576400000 , 17] , [ 1070168400000 , 16] , [ 1072846800000 , 16] , [ 1075525200000 , 14] , [ 1078030800000 , 18] , [ 1080709200000 , 11] , [ 1083297600000 , 10] , [ 1085976000000 , 14] , [ 1088568000000 , 18] , [ 1091246400000 , 16] , [ 1093924800000 , 14] , [ 1096516800000 , 14] , [ 1099195200000 , 14] , [ 1101790800000 , 15] , [ 1104469200000 , 13] , [ 1107147600000 , 11] , [ 1109566800000 , 11] , [ 1112245200000 , 11] , [ 1114833600000 , 19] , [ 1117512000000 , 11] , [ 1120104000000 , 11] , [ 1122782400000 , 12] , [ 1125460800000 , 11] , [ 1128052800000 , 13] , [ 1130734800000 , 12] , [ 1133326800000 , 13] , [ 1136005200000 , 16] , [ 1138683600000 , 14] , [ 1141102800000 ,12] , [ 1143781200000 , 16] , [ 1146369600000 , 16] , [ 1149048000000 , 16] , [ 1151640000000 , 13] , [ 1154318400000 , 13] , [ 1156996800000 , 12] , [ 1159588800000 , 17] , [ 1162270800000 , 19] , [ 1164862800000 , 18] , [ 1167541200000 , 12] , [ 1170219600000 , 12] , [ 1172638800000 , 12] , [ 1175313600000 , 14] , [ 1177905600000 , 16] , [ 1180584000000 , 16] , [ 1183176000000 , 18] , [ 1185854400000 , 18] , [ 1188532800000 , 19] , [ 1191124800000 , 14] , [ 1193803200000 , 16] , [ 1196398800000 , 15] , [ 1199077200000 , 17] , [ 1201755600000 , 17] , [ 1204261200000 , 16] , [ 1206936000000 , 19] , [ 1209528000000 , 18] , [ 1212206400000 , 14] , [ 1214798400000 , 15] , [ 1217476800000 , 15] , [ 1220155200000 , 15] , [ 1222747200000 , 13] , [ 1225425600000 , 11] , [ 1228021200000 , 12] , [ 1230699600000 , 15] , [ 1233378000000 , 14] , [ 1235797200000 , 15] , [ 1238472000000 , 13] , [ 1241064000000 , 14] , [ 1243742400000 , 14] , [ 1246334400000 , 16] , [ 1249012800000 , 15] , [ 1251691200000 , 16] , [ 1254283200000 , 18] , [ 1256961600000 , 16] , [ 1259557200000 , 19] , [ 1262235600000 , 12] , [ 1264914000000 , 13] , [ 1267333200000 , 15] , [ 1270008000000 , 16] , [ 1272600000000 , 15] , [ 1275278400000 , 14] , [ 1277870400000 , 15] , [ 1280548800000 , 19] , [ 1283227200000 , 17] , [ 1285819200000 , 19] , [ 1288497600000 , 18] , [ 1291093200000 , 18] , [ 1293771600000 , 12] , [ 1296450000000 , 15] , [ 1298869200000 , 18] , [ 1301544000000 , 12] , [ 1304136000000 , 15] , [ 1306814400000 , 13] , [ 1309406400000 , 13] , [ 1312084800000 , 13] , [ 1314763200000 , 11] , [ 1317355200000 , 11] , [ 1320033600000 , 16] , [ 1322629200000 , 17] , [ 1325307600000 , 10] , [ 1327986000000 , 19] , [ 1330491600000 , 20] , [ 1333166400000 , 19] , [ 1335758400000 , 19]]
@@ -269,7 +303,7 @@
 	var timeline= [  1025409600000 ,  1028088000000 ,  1030766400000 , 1033358400000 ,  1036040400000 ,1038632400000 ,  1041310800000 , 1043989200000 ,  1046408400000 ,  1049086800000 ,  1051675200000 , 1054353600000 , 1056945600000 , 1059624000000 , 1062302400000 ,  1064894400000 ,  1067576400000 ,1070168400000 ,  1072846800000 ,  1075525200000 ,  1078030800000 ,  1080709200000 , 1083297600000 ,  1085976000000 , 1088568000000 ,  1091246400000 ,  1093924800000 ,  1096516800000 ,  1099195200000 ,  1101790800000 ,  1104469200000 ,  1107147600000 ,  1109566800000 ,1112245200000 , 1114833600000 ,  1117512000000 ,  1120104000000 ,  1122782400000 ,  1125460800000 ,  1128052800000 ,  1130734800000 ,  1133326800000 ,  1136005200000 , 1138683600000 ,  1141102800000 , 1143781200000 ,  1146369600000 , 1149048000000 ,  1151640000000 ,  1154318400000 ,  1156996800000 ,  1159588800000 ,  1162270800000 ,  1164862800000 ,  1167541200000 ,  1170219600000 ,  1172638800000 ,  1175313600000 , 1177905600000 ,  1180584000000 ,  1183176000000 ,  1185854400000 ,  1188532800000 ,  1191124800000 ,  1193803200000 ,  1196398800000 ,  1199077200000 , 1201755600000 , 1204261200000 ,  1206936000000 ,  1209528000000 ,  1212206400000 ,  1214798400000 ,  1217476800000 ,  1220155200000 ,  1222747200000 ,  1225425600000 ,  1228021200000 ,  1230699600000 ,  1233378000000 ,  1235797200000 ,  1238472000000 ,  1241064000000 ,  1243742400000 ,  1246334400000 ,  1249012800000 ,  1251691200000 ,  1254283200000 ,  1256961600000 ,  1259557200000 ,  1262235600000 ,  1264914000000 ,  1267333200000 ,  1270008000000 ,  1272600000000 ,  1275278400000 ,  1277870400000 ,  1280548800000 ,  1283227200000 ,  1285819200000 ,  1288497600000 ,  1291093200000 ,  1293771600000 ,  1296450000000 ,  1298869200000 ,  1301544000000 ,  1304136000000 ,  1306814400000 ,  1309406400000 ,  1312084800000 ,  1314763200000 ,  1317355200000 ,  1320033600000 ,  1322629200000 ,  1325307600000 , 1327986000000 , 11330491600000 ,  1333166400000 ,  1335758400000 ];
 
 
-})();
+/*})();*/
 
 
 
